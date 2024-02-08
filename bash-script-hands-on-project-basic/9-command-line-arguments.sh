c40152960d3e919a8e90d3ce100b24dea295028a
Mohammad Javad Tabari <mohammadJavadTabari1024@outlook.com>
2024-02-08 21:03:30 +0330

    command-line-arguments

---
+++ bash-script-hands-on-project-basic/9-command-line-arguments.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "script name : $0"
+echo "first argument name : $1"
+echo "second argument name : $2"
+echo "total numbers of arguments : $#"
+echo "all arguments : " "$@"
+
+# to excute this in terminal => bash 9-command-line-arguments.sh arg1 arg2
