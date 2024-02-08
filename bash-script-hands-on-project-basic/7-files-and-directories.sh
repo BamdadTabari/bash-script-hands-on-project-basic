de5eddae5b505e10fb998160e84a1883869987d5
Mohammad Javad Tabari <mohammadJavadTabari1024@outlook.com>
2024-02-08 19:03:54 +0330

    create , write, append, read to file

---
+++ bash-script-hands-on-project-basic/7-files-and-directories.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# create , write, append, read, copy, move, rename, delete 
+
+#create
+touch file1.txt
+
+#write
+echo " write to file " > file1.txt
+
+#append
+echo " append to file " >> file1.txt
+
+#read
+cat file1.txt
