4391b9a6add2f858929f706ab8db46b922a0a51d
Mohammad Javad Tabari <mohammadJavadTabari1024@outlook.com>
2024-02-08 19:11:55 +0330

    copy, move, rename, delete  file

--- bash-script-hands-on-project-basic/7-files-and-directories.sh
+++ bash-script-hands-on-project-basic/7-files-and-directories.sh
@@ -13,3 +13,12 @@ echo " append to file " >> file1.txt
 
 #read
 cat file1.txt
+
+#copy
+cp file1.txt file1_copy.txt
+
+#move, rename => for move you should give the address too
+mv file1.txt file2.txt
+
+#remove
+rm file1.txt
